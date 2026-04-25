40a3261f8673193e06b2cec7391ea967a4360b56
bari-daniel <788danibari@gmail.com>
2026-04-25 18:33:16 +0200

    update auth headers and csrf middleware

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use App\Models\User;
+use Faker\Generator as Faker;
+use Faker\Factory as FakerFactory;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -12,6 +14,8 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
+    protected $model = User::class;
+
     /**
      * The current password being used by the factory.
      */
@@ -22,11 +26,16 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
+    protected function faker(): Faker
+    {
+        return $this->faker ??= FakerFactory::create();
+    }
+
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker()->name(),
+            'email' => $this->faker()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
